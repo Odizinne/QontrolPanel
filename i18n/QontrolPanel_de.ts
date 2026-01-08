723ad6acf9a7dd228606986a5b231f09d833687c
Flora Aubry <flora.aubry@proton.me>
2026-01-08 23:15:48 +0100

    removed translator tag for de file

--- i18n/QontrolPanel_de.ts
+++ i18n/QontrolPanel_de.ts
@@ -1,7 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1" language="de_DE">
-<translator>Christian Lauinger (christian@lauinger-clan.de)</translator>
 <context>
     <name>AppearancePane</name>
     <message>
